65519669314678e2e8165939035160a558c07b81
GoldyL <vitaliyfnaf@gmail.com>
2024-11-17 00:56:15 +0200

    sry

--- src/main/java/Hivens/hdu/Common/event/cutscene/CutsceneMode.java
+++
@@ -1,7 +0,0 @@
-package Hivens.hdu.Common.event.cutscene;
-
-public enum CutsceneMode {
-    SINGLE_PLAY,    // Воспроизведение один раз
-    LOOP,           // Зацикленное воспроизведение
-    REPEAT_LIMITED  // Ограниченное число повторов
-}
